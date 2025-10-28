07d095c825f796227e1ed83d518b23cc2bfc7fb8
koi <103864356+koish1t@users.noreply.github.com>
2025-10-28 17:15:56 -0400

    wip monogame port stuff

---
+++ S4EP1.Monogame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+internal static class Program
+{
+    [STAThread]
+    private static void Main()
+    {
+        using (var game = new Sonic4Ep1())
+        {
+            game.Run();
+        }
+    }
+}
